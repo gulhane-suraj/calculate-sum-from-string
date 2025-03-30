593a3f98edf3db63b06477334cd19b1682105ad7
SS_Apple <ss_apple@SS-Apples-MacBook-Pro.local>
2025-03-30 16:41:22 +0530

    develop Passed  case: should return the number itself for a single number

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 export const add = (numbers: string): number => {
     if (!numbers) return 0;
+    if(numbers.length === 1) return parseInt(numbers, 10);
 
     return 0; // Default return value for other cases
 };
