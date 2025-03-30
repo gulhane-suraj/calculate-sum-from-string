f291dae91a94b115d0046a5eeaf83a30e856efb6
SS_Apple <ss_apple@SS-Apples-MacBook-Pro.local>
2025-03-30 16:52:08 +0530

    develop Failed case: should handle newlines as delimiters, should support custom delimiters

--- src/utils/add.test.ts
+++ src/utils/add.test.ts
@@ -19,10 +19,16 @@ describe('add function', () => {
         expect(add("1,2,3,4,5")).toBe(15);
         expect(add("10,20,30,40,50")).toBe(150);
     });
+    
 
     test('should handle newlines as delimiters', () => {
         expect(add("1\n2,3")).toBe(6);
         expect(add("10\n20\n30")).toBe(60);
+    });
+
+    test('should support custom delimiters', () => {
+        expect(add("//;\n1;2;3")).toBe(6);
+        expect(add("//|\n10|20|30")).toBe(60);
     });
 
 });
