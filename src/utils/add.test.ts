de975b78859a1785f9e011433014c914792470d3
SS_Apple <ss_apple@SS-Apples-MacBook-Pro.local>
2025-03-30 16:49:04 +0530

    develop Passed case: should handle an unknown amount of numbers

--- src/utils/add.test.ts
+++ src/utils/add.test.ts
@@ -14,5 +14,10 @@ describe('add function', () => {
         expect(add("1,2")).toBe(3);
         expect(add("10,20")).toBe(30);
     });
-    
+
+    test('should handle an unknown amount of numbers', () => {
+        expect(add("1,2,3,4,5")).toBe(15);
+        expect(add("10,20,30,40,50")).toBe(150);
+    });
+
 });
