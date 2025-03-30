e48a13601d3064b151d4728ee3fb79550ce64098
SS_Apple <ss_apple@SS-Apples-MacBook-Pro.local>
2025-03-30 16:50:27 +0530

    develop Passed case: should handle newlines as delimiters

--- src/utils/add.test.ts
+++ src/utils/add.test.ts
@@ -20,4 +20,9 @@ describe('add function', () => {
         expect(add("10,20,30,40,50")).toBe(150);
     });
 
+    test('should handle newlines as delimiters', () => {
+        expect(add("1\n2,3")).toBe(6);
+        expect(add("10\n20\n30")).toBe(60);
+    });
+
 });
