22ffab0267c86c78e77e95930ab3c421913a8134
SS_Apple <ss_apple@SS-Apples-MacBook-Pro.local>
2025-03-30 16:47:42 +0530

    develop Passed case: should return the sum of two comma-separated numbers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,10 @@
 export const add = (numbers: string): number => {
     if (!numbers) return 0;
     if(numbers.length === 1) return parseInt(numbers, 10);
+    const numArray = numbers.split(',').map((num) => {
+        const parsed = parseInt(num, 10);
+        return parsed;
+    });
 
-    return 0; // Default return value for other cases
+    return numArray.reduce((sum, num) => sum + num, 0); // Calculate and return the sum
 };
